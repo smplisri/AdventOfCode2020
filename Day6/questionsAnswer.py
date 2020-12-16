12b06508c840f3e44be54e4f5c785fb1d75d35ff
Srinivasa kalyan Sozhavaram <s.srinivasakalyan@gmail.com>
2020-12-15 23:50:14 -0500

    Completion of day 6 part 2

--- Day6/questionsAnswer.py
+++ Day6/questionsAnswer.py
@@ -6,8 +6,23 @@ from standalone.udfs import *
 
 fh = inputFileHandler(__file__, "Input.txt")
 lines_list = fh.readlines()
-delimiter = "\n"
+delimiter = "\n"    
 
-unique_ans = list(map(lambda x: len(set(list(x))), lineSepGroupFormatter(lines_list, delimiter, '')))
+formatted_lines = lineSepGroupFormatter(lines_list, delimiter, '')
 
-print('Total number of answers --> ' + str(reduce(lambda x, y: x+y, unique_ans)))
+answers = list(map(lambda x: list(x), formatted_lines))
+unique_answers = list(map(lambda x: set(list(x)), formatted_lines))
+
+unique_answers_count = reduce(lambda x, y: len(set(x)) + len(set(y)) if type(x) is list else x + len(set(y)), answers)
+print(f'Total number of answers --> {unique_answers_count}')
+
+common_answers_count = 0
+dict_format_lines = lineSepGroupFormatterDict(lines_list, delimiter, '')
+for iterator in range(1, len(dict_format_lines)+1):
+    group_counts = len(dict_format_lines["group" + str(iterator)])
+    unique_charset = set(list(''.join(dict_format_lines["group" + str(iterator)])))
+    for char in unique_charset:
+        if sum(char in item for item in dict_format_lines["group" + str(iterator)]) == group_counts:
+            common_answers_count = common_answers_count + 1
+
+print(f"Total number of common answers --> {common_answers_count}")
--- standalone/udfs.py
+++ standalone/udfs.py
@@ -10,3 +10,17 @@ def inputFileHandler(script_file_name, input_file_name):
 def lineSepGroupFormatter(lines_list, delimiter, join_char):
     formatted_list = map(lambda x: x.strip() if x != delimiter else x, lines_list)
     return join_char.join(formatted_list).split(join_char + delimiter + join_char)
+
+def lineSepGroupFormatterDict(lines_list, delimiter, join_char):
+    iterator, answerset, interim_data = 1, {}, []
+    formatted_list = list(map(lambda x: x.strip() if x != delimiter else x, lines_list))
+    formatted_list.append(delimiter)
+    for item in formatted_list:
+        if item.strip() == "":
+            answerset["group" + str(iterator)] = interim_data
+            interim_data = []
+            iterator = iterator + 1
+        else:
+            interim_data.append(item)
+    
+    return answerset
